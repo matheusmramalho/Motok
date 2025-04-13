11597d34a3016ab1a1f6a39b10e659c2753156ec
Matheus de Moraes Ramalho <matheusramalho@hotmail.com.br>
2025-04-13 13:52:15 -0300

    Add project files.

---
+++ src/MatheusR.Motok.Domain/Entities/Motorcycle.cs
@@ -0,0 +1,9 @@
+﻿namespace MatheusR.Motok.Domain.Entities;
+public class Motorcycle
+{
+    public Guid Id { get; set; }
+    public string Identifier { get; set; }
+    public int Year { get; set; }
+    public string Model { get; set; }
+    public string LicencePlate { get; set; }
+}
